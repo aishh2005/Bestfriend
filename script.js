2e26e6928bec0f1d445f0b900766af1eb2ec226f
aishh2005 <aishwaryahjuwlekar@gmail.com>
2024-11-01 23:56:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function showFriendshipContent() {
+    document.getElementById("initial-view").style.display = "none";
+    document.getElementById("friendship-content").style.display = "block";
+}
+
